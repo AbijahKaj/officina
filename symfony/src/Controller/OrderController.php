2153580f168774282b2248e1365d83f253429d30
tamar peled <tamarpe@gmail.com>
2020-08-08 17:50:49 +0200

    Fix validations with CSRF, fixing coding standards

--- symfony/src/Controller/OrderController.php
+++ symfony/src/Controller/OrderController.php
@@ -41,7 +41,7 @@ class OrderController extends AbstractController
         $order->setOfficeId($id);
         $user = $this->getUser();
         $order->setBookedBy($user->getId());
-        $form = $this->createForm(OrderType::class, $order, ['csrf_protection' => false, 'validation_groups'=>'new_order']);
+        $form = $this->createForm(OrderType::class, $order, ['validation_groups' => 'new_order']);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
