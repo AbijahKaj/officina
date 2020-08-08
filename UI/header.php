cdb7949987c686ecc7ee687d90bed594f453773c
ankitsharmaongithub <ankitsharma101299@gmail.com>
2020-08-08 20:52:26 +0530

    Updated Header and footer

--- UI/header.php
+++ UI/header.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Giftechies">
-    <meta name="author" content="Giftechies">
+    <meta name="description" content="Team Hyper Gram">
+    <meta name="author" content="Team Hyper Gram(Ankit Kumar Sharma, ">
     <title>Officina | Team Hyper Gram</title>
     <!-- Favicon Icon -->
     <link rel="icon" type="image/png" href="img/favicon.png">
