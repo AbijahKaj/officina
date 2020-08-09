c32883b9db72f856be3a93df93c99bf25cff14ff
ankitsharmaongithub <ankitsharma101299@gmail.com>
2020-08-10 00:26:25 +0530

    Added slider

--- UI/office_single.php
+++ UI/office_single.php
@@ -1,5 +1,64 @@
 <?php include('header.php') ?>
 
+<!-- Property Single Slider header -->
+<section class="samar-slider">
+    <div id="samarslider" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#samarslider" data-slide-to="0" class="active"></li>
+            <li data-target="#samarslider" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active slider-one">
+                <div class="overlay"></div>
+            </div>
+            <div class="carousel-item slider-two">
+                <div class="overlay"></div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#samarslider" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#samarslider" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="property-single-title">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8">
+                    <h1 class="text-white">House in Kent Street</h1>
+                    <h6 class="text-white"><i class="mdi mdi-home-map-marker"></i> 250-260 3rd St, Hoboken, NJ
+                        07030, USA</h6>
+                </div>
+                <div class="col-lg-4 col-md-4 text-right">
+                    <h6 class="mt-2 text-white">For Rent</h6>
+                    <h2 class="text-warning">$25,000 <small>/month</small></h2>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-lg-8 col-md-8">
+                    <p class="mt-1 mb-0 text-white"><strong>Property ID</strong> : 533566 &nbsp;&nbsp; <strong>Add
+                            to favorites</strong> <i class="mdi mdi-heart text-danger"></i></p>
+                </div>
+                <div class="col-lg-4 col-md-4 text-right">
+                    <div class="footer-social">
+                        <span class="text-info">Share :</span> &nbsp;
+                        <a class="btn-facebook" href="#"><i class="mdi mdi-facebook"></i></a>
+                        <a class="btn-twitter" href="#"><i class="mdi mdi-twitter"></i></a>
+                        <a class="btn-instagram" href="#"><i class="mdi mdi-instagram"></i></a>
+                        <a class="btn-whatsapp" href="#"><i class="mdi mdi-whatsapp"></i></a>
+                        <a class="btn-messenger" href="#"><i class="mdi mdi-facebook-messenger"></i></a>
+                        <a class="btn-google" href="#"><i class="mdi mdi-google"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Property Single Slider header --
 
 
 <?php include('footer.php') ?>
